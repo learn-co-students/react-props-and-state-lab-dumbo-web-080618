eb89678213aacf663bc3be9b506ad7c55378b205
github username <github email address>
2018-10-10 19:13:23 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,32 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  // onAdoptPet = () => {
+  //   // console.log(this.props.singlePet.id.)
+  //   this.setState([this.state.pets]: this.props.singlePe)
+  //
+  // }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+
+            {this.props.singlePet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.singlePet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.singlePet.age}</p>
+            <p>Weight: {this.props.singlePet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.props.isAdopted === true ? <button className="ui disabled button">Already adopted</button> : <button className="ui primary button" onClick={() => this.props.onAdoptPet(this.props.singlePet.id)}>Adopt pet</button>}
+
         </div>
       </div>
     )
